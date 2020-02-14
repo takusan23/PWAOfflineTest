a05a9ffa13728f0bdda1e83c0c5b851053d190d7
takusan23 <takumu1200@gmail.com>
2020-02-15 02:29:02 +0900

    sw

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
- 
+const STATIC_DATA = [
+    'index.html',
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('cache_v1').then(function (cache) {
+            return cache.addAll(STATIC_DATA);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log(event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
